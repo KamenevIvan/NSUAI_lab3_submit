f4235a662c4d872908250f654047b53cc384e12e
Ivan <kamenev2003kamenev@gmail.com>
2025-06-18 13:03:13 +0700

    initial commit

---
+++ Lab3/main.py
@@ -0,0 +1,37 @@
+import argparse
+import cv2
+import numpy as np
+import os
+from pathlib import Path
+import torch
+from yolov5.detect import run
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Predict bounding boxes on the image.')
+    parser.add_argument('input_path', type=str, help='Path to input file or folder.')
+    parser.add_argument('--output_path', type=str, default='./output', help='Path to output files.')
+    parser.add_argument('--model_weights', type=str, default='./yolov5/runs/train/drone_human_det4/weights/best.pt', help='Path to model weights.')
+    parser.add_argument('--confidence', type=float, default=0.5, help='Confidence threshold.')
+    parser.add_argument('--save_txt', action='store_true', help='Save detection results to *.txt files')
+
+    args = parser.parse_args()
+
+    run(
+        weights=args.model_weights,
+        source=args.input_path,
+        project=args.output_path,
+        name='',
+        exist_ok=True,
+        conf_thres=args.confidence,
+        imgsz=(640, 640),
+        save_txt=args.save_txt,
+        save_conf=True,
+        save_crop=False,
+        nosave=False
+    )
+
+
+
+if __name__ == "__main__":
+    main()
